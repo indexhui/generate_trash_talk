053ae9a5d8569cfc4fee195e308b6ba6f3f265b5
Hugh <indexhui@gmail.com>
2021-06-05 10:42:50 +0800

    first commit

---
+++ READE.md
@@ -0,0 +1,34 @@
+# AC 學期 2-3 ｜ A5【選修】幹話產生器
+
+
+## 安裝套件
+
+- Node.js
+- Express: 4.17.1
+- Express-Handlebars: 5.3.2
+- handlebars-helpers:
+
+## 基本功能
+
+- User 可以選擇工程師、設計師、或創業家 產生幹話
+
+## Getting Started
+Clone to your local computer
+```
+$ git clone https://github.com/indexhui/generate_trash_talk.git
+```
+Install by npm
+```
+$ npm install
+```
+Execute
+```
+$ npm run dev
+```
+Terminal show the message
+```
+Express is running on localhost:3000
+```
+Now you can browse the website on
+```
+http://localhost:3000
---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const hbshelpers = require('handlebars-helpers')();
+const port = 3000
+
+const exphbs = require('express-handlebars')
+const generateTrashTalk = require('./generate_trash_talk')
+
+app.engine('hbs', exphbs({ default: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// setting static files
+app.use(express.static('public'))
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(express.urlencoded({ extended: true }))
+
+// setting routes
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const character = req.body.character
+  const rubbish = generateTrashTalk(character)
+  console.log(character, rubbish)
+  res.render('index', { character, rubbish })
+})
+
+// starts the express server and listening for connections.
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}.`)
+})
